613c99b33900c4e4baf4fd5a9227436bb80a88bd
sls_grim <alexanderh@stunlockstudios.com>
2019-03-07 14:40:27 +0100

    Lecture 13

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 
 const getNotes = require('./notes');
 
-console.log(chalk.bold.inverse.rgb(10, 100, 200)('Hello!'));
+console.log(chalk.green('Tjena!'));
 
 console.log(getNotes());
 
