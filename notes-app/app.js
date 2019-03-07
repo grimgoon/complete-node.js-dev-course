2f94037bc7b0804b23e71a5339f29b557ae53037
sls_grim <alexanderh@stunlockstudios.com>
2019-03-07 14:21:46 +0100

    Lecture 11

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,12 @@
+const validator = require('validator');
+
 const getNotes = require('./notes');
 
 console.log(getNotes());
+
+
+console.log(validator.isURL('battlerite.com'));
+
 
 // const add = require('./utils');
 // console.log(add(2,3));
