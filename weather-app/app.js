3086ea8ffa5aacce06fac4d49ae1e5fad58081c1
sls_grim <alexanderh@stunlockstudios.com>
2019-03-18 08:57:47 +0100

    Lecture 31

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-    console.log('2 Second Timer');
-},2000);
+const url  = 'https://api.darksky.net/forecast/db0a1be86676a8f0071e35dc0cac1c11/37.8267,-122.4233';
 
-setTimeout(() => {
-    console.log('Meep');
-},0);
-
-console.log('Stopping');
+request(url, (error,response,body) => {
+    const data = JSON.parse(body);
+    console.log(data.currently);
+});
