480cc67e684d471f961b2c0383bd1098d07140dd
sls_grim <alexanderh@stunlockstudios.com>
2019-03-18 09:23:30 +0100

    Lecture 32

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,11 @@
 const request = require('request');
 
-const url  = 'https://api.darksky.net/forecast/db0a1be86676a8f0071e35dc0cac1c11/37.8267,-122.4233';
+const url  = 'https://api.darksky.net/forecast/db0a1be86676a8f0071e35dc0cac1c11/37.8267,-122.4233?units=si&lang=sv';
 
-request(url, (error,response,body) => {
-    const data = JSON.parse(body);
-    console.log(data.currently);
+request({url : url, json : true}, (error,response,body) => {
+    console.log(body.daily.data[0].summary);
+    console.log("It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain.");
+
 });
+
+
