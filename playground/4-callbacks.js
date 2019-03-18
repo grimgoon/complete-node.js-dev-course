e1452b6f30652df3c4f6d0c194e13e8c7cc2466c
sls_grim <alexanderh@stunlockstudios.com>
2019-03-18 14:08:39 +0100

    lecture 35

---
+++ playground/4-callbacks.js
@@ -0,0 +1,44 @@
+// setTimeout(() => {
+//     console.log('Two Seconds Are Up!');
+// }, 2000);
+
+
+// const names = ['Andrew','Meep','Beep'];
+
+// const shortNames = names.filter(name => name.length <= 4);
+
+
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 112,
+//             longitude: 0
+//         }
+
+//         callback(data);
+
+//     }, 2000)
+// }
+
+// const data = geocode('philadellalaa', (data) => {
+//     console.log(data);
+// });
+
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+const add = (num1, num2, callback) => {
+    setTimeout(() => {
+        callback(num1+num2);
+    },2000)
+}
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
