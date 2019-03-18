2983b9954ab624226b7357dcec04b9d9a3a185e3
sls_grim <alexanderh@stunlockstudios.com>
2019-03-18 14:27:27 +0100

    Lecture 36

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
-const request = require('request');
+const geocode = require('./utils/geocode');
 
 // const urlDarksky  = 'https://api.darksky.net/forecast/db0a1be86676a8f0071e35dc0cac1c11/37.8267,-122.4233?units=si&lang=sv';
-// const urlMapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?limit=2&access_token=pk.eyJ1IjoiZ3JpbWdvb24iLCJhIjoiY2p0ZTM5amI2MWFqNTRhcDh1aGdpbnRkbSJ9.4-Xjwa6vruF5lYpvKI0uYw&limit=1'
+
 
 // request({url : urlDarksky, json : true}, (error,response,body) => {
 
@@ -15,13 +15,22 @@ const request = require('request');
 //     }
 // });
 
-request({url : urlMapbox, json : true}, (error,response,body) => {
-    if(error) {
-        console.log('Meep, thing are wrong');
-    } else if(body.body.features.length === 0) {
-        console.log('Meep. The request is having troubles.');
-    } else {
-        console.log(body.features[0].center[0]); //Longitude
-        console.log(body.features[0].center[1]); //Latitude
-    }
+// request({url : urlMapbox, json : true}, (error,response,body) => {
+//     if(error) {
+//         console.log('Meep, thing are wrong');
+//     } else if(body.body.features.length === 0) {
+//         console.log('Meep. The request is having troubles.');
+//     } else {
+//         console.log(body.features[0].center[0]); //Longitude
+//         console.log(body.features[0].center[1]); //Latitude
+//     }
+// });
+
+
+
+
+
+geocode('Stockholm', (error,data) => {
+    console.log('Error: ', error);
+    console.log('Data: ', data);
 });
