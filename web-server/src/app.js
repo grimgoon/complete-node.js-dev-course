d6c97b3c1d673b78702d987c157227683c92a6ee
sls_grim <alexanderh@stunlockstudios.com>
2019-03-19 09:05:57 +0100

    Lecture 45

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,15 @@
+const path = require('path');
+
 const express = require('express');
+
+console.log(__dirname);
+console.log(path.join(__dirname, '../public'));
 
 const app = express();
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>');
-});
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'Alexander',
-        age: 24,
-    });
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>');
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get('/weather', (req, res) => {
     res.send({
