cea2cd834fc075120415702c90b75dc12a1b3837
sls_grim <alexanderh@stunlockstudios.com>
2019-03-19 08:51:08 +0100

    Lecture 44

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,22 +3,26 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Hello Express!!!');
+    res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('Hello, help!!');
+    res.send({
+        name: 'Alexander',
+        age: 24,
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('Hello, about!!');
+    res.send('<h1>About</h1>');
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Weather Page!!');
+    res.send({
+        forecast: 'Cloudy',
+        location: 'Stockholm',
+    });
 });
-
-
 
 app.listen(3000, () => {    
     console.log('Server is up on port 3000.');
