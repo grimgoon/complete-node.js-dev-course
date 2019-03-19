eff59b53d337663826fc9a2b2df9bc4b5e0e3642
sls_grim <alexanderh@stunlockstudios.com>
2019-03-19 15:08:24 +0100

    Lecture 49-52

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,1 @@
-console.log('This is a message, client side stuff is loaded');
+console.log('Client side javascript file is loaded!')
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,50 +1,67 @@
-const path = require('path');
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
 
-const express = require('express');
+const app = express()
 
-console.log(__dirname);
-console.log(path.join(__dirname, '../public'));
-
-const app = express();
-
-// Define paths for express config
-const viewsPath = path.join(__dirname, '../templates');
-const publicDirectoryPath = path.join(__dirname, '../public');
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-// setup static directory to serve
-app.use(express.static(publicDirectoryPath));
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
-app.get('', (req,res) => {
+app.get('', (req, res) => {
     res.render('index', {
-        title : 'Weather App',
-        name : 'Alexander H',
-    });
-});
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
+})
 
-app.get('/about', (req,res) => {
+app.get('/about', (req, res) => {
     res.render('about', {
-        title : 'About Page',
-        name : 'Alexander H',
-    });
-});
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
 
-app.get('/help', (req,res) => {
+app.get('/help', (req, res) => {
     res.render('help', {
-        message : 'This is a help message',
-    });
-});
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
-        forecast: 'Cloudy',
-        location: 'Stockholm',
-    });
-});
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
 
-app.listen(3000, () => {    
-    console.log('Server is up on port 3000.');
-});
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
