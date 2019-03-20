480e096429de0189c62a8eaa148237e7bf7ba3d7
sls_grim <alexanderh@stunlockstudios.com>
2019-03-20 09:28:24 +0100

    Lecture 57

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,10 @@
-console.log('Client side javascript file is loaded!')
+console.log('Client side javascript file is loaded!');
+
+fetch('http://localhost:3000/weather/?address=HohoHaha').then((res) => {
+    res.json().then((data) => {
+        if(data.error) {
+            return console.log("Error: " + data.error);
+        }
+        console.log(data);
+    })
+});
