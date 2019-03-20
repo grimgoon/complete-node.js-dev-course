81998d933530c25dafcb7b734de175722ffaf18a
sls_grim <alexanderh@stunlockstudios.com>
2019-03-20 10:36:27 +0100

    Lecture 59

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,21 +1,28 @@
 console.log('Client side javascript file is loaded!');
 
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+const messageThree = document.querySelector('#message-3');
 
-
-const search = document.querySelector('input');
-const formElement = document.querySelector('form');
-
-formElement.addEventListener('submit', (event) => {
-    event.preventDefault();
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
     const location = search.value;
 
-    fetch('http://localhost:3000/weather/?address=' + location).then((res) => {
-        res.json().then((data) => {
-            if(data.error) {
-                return console.log("Error: " + data.error);
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast.summary;
+                messageThree.textContent = data.forecast.temperature + 'C';
             }
-            console.log(data);
         });
     });
 });
