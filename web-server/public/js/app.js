f81edb8d330aaab30f5eb7d9ae40173b88b9fd6b
sls_grim <alexanderh@stunlockstudios.com>
2019-03-20 10:10:44 +0100

    Lecture 58

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,10 +1,21 @@
 console.log('Client side javascript file is loaded!');
 
-fetch('http://localhost:3000/weather/?address=HohoHaha').then((res) => {
-    res.json().then((data) => {
-        if(data.error) {
-            return console.log("Error: " + data.error);
-        }
-        console.log(data);
-    })
+
+
+const search = document.querySelector('input');
+const formElement = document.querySelector('form');
+
+formElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const location = search.value;
+
+    fetch('http://localhost:3000/weather/?address=' + location).then((res) => {
+        res.json().then((data) => {
+            if(data.error) {
+                return console.log("Error: " + data.error);
+            }
+            console.log(data);
+        });
+    });
 });
