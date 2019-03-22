1fe0b2249dcbd7c43802a6d4db19c9d00d324c75
grimgoon <alexander@hermansson.us>
2019-03-22 10:50:09 +0100

    Lecture 76

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,9 +13,56 @@ MongoClient.connect(connectionURL,{ useNewUrlParser : true}, (error, client) => 
     
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name : 'Andrew',
-        age : 27,
+    // db.collection('users').insertOne({
+    //     name : 'Andrew',
+    //     age : 27,
+    // },(error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert user');
+    //     }
+        
+    //     console.log(result.ops);
+    // });
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28,
+    //     },
+    //     {
+    //         name : 'Meep',
+    //         age : 26,
+    //     },  
+    // ], (error, result) => {
+    //     if(error) {
+    //         console.log('Unable to insert documents');
+    //     }
+
+    //     console.log(result.ops);
+    // });
+
+    db.collection('tasks').insertMany([
+        {
+            description : 'Clean kitchen',
+            completed : false,
+        },
+        {
+            description : 'Fix toilet',
+            completed : false,
+        },
+        {
+            description : 'Meep the Morp',
+            completed : true,
+        },
+    ], (error, response) => {
+        if(error) {
+            return console.log('Unable to insert documents');
+        }  
+
+        console.log(response.ops);
+
     });
+
+
 
 });
