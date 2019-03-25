aa78b606fa6be2ff657f04374b22f9fc13d8add9
grimgoon <alexander@hermansson.us>
2019-03-25 10:16:55 +0100

    Lecture 86

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -2,70 +2,79 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser : true,
-    useCreateIndex : true,
+    useNewUrlParser: true,
+    useCreateIndex: true
 });
 
 const User = mongoose.model('User', {
-    name : {
+    name: {
         type: String,
-        required : true,
-        trim : true,
+        required: true,
+        trim: true
     },
-    age : {
-        type: Number,
-        default : 0,
-        validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        }
-    },
-    email : {
+    email: {
         type: String,
-        required : true,
+        required: true,
         trim: true,
         lowercase: true,
         validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('String must be a corret email');
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
             }
         }
     }
 });
 
-
-const me = new User({
-    name : ' Meep Maap',
-    email: 'Moop@mOop.su '
-});
-
-me.save().then(response => {
-    console.log(me);
-    console.log(response);
-}).catch(error => {
-    console.log(error);
-});
-
-
-const Tasks = mongoose.model('Tasks', {
-    description : {
-        type: String
-    },
-    completed : {
-        type: Boolean
-    },
-});
-
-
-// const task = new Tasks({
-//     description : 'Learn about Tasks',
-//     completed: false,
+// const me = new User({
+//     name: '   Andrew  ',
+//     email: 'MYEMAIL@MEAD.IO   ',
+//     password: 'phone098!'
 // });
 
-// task.save().then(response => {
-//     console.log(task);
-//     console.log(response);
-// }).catch(error => {
-//     console.log(error);
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
 // });
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const task = new Task({
+    description: '  Eat lunch'
+});
+
+task.save().then(() => {
+    console.log(task)
+}).catch((error) => {
+    console.log(error)
+});
