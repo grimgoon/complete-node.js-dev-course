0a5681511e8022080b8d2618838892f16f6f22fe
grimgoon <alexander@hermansson.us>
2019-03-25 16:58:23 +0100

    Lecture 96

--- task-manager/playground/promise-chaining2.js
+++ task-manager/playground/promise-chaining2.js
@@ -1,11 +1,25 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5c98c615dd8b3f3cf8088e7d').then((res) => {
-    console.log(res);
-    return Task.countDocuments({completed : false});
-}).then((countCompleted) => {
-    console.log(countCompleted);
+// Task.findByIdAndDelete('5c98c615dd8b3f3cf8088e7d').then((res) => {
+//     console.log(res);
+//     return Task.countDocuments({completed : false});
+// }).then((countCompleted) => {
+//     console.log(countCompleted);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+const deleteTaskAndCount = async () => {
+    await Task.findByIdAndDelete('5c98c615dd8b3f3cf8088e7d');
+    const count = await Task.countDocuments({completed: false});
+
+    return count;
+}
+
+deleteTaskAndCount().then((countSum) => {
+    console.log(countSum);
 }).catch((e) => {
     console.log(e);
 });
+
