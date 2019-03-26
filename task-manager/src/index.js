4b60df5a7edc63076c84fbb6019937ab0f93c291
grimgoon <alexander@hermansson.us>
2019-03-26 10:40:16 +0100

    Lecture 103

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -17,3 +17,18 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on port: ' + port);
 });
+
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+    const passwd = 'Red12345';
+    const hashedPassword = await bcrypt.hash(passwd,8);
+
+    console.log(passwd, hashedPassword);
+
+    const isMatch = await bcrypt.compare(passwd, '$2a$08$H6AMUdAiHB.g8gSLp2PbH.Fuu1aMf3Y3FD.OrLA6E5Pw/F3LriKR.');
+
+    console.log(isMatch);
+}
+
+myFunction();
