11c17d4b418dc12c7fbc9b532b39aa6b9d6563c7
grimgoon <alexander@hermansson.us>
2019-05-28 09:08:27 +0200

    Lecture 139

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const {calculateTip} = require('../src/math')
+const {calculateTip, celsiusToFahrenheit, fahrenheitToCelsius} = require('../src/math')
 
 test('Should calculate total with tip',() => {
     const total = calculateTip(10,.3);
@@ -16,6 +16,20 @@ test('Should calculate tip with default tip', () => {
     const total = calculateTip(10);
     expect(total).toBe(12.5);
 })
+
+test('Should convert celsius to fahrenheit', () => {
+    const degree = celsiusToFahrenheit(0);
+    expect(degree).toBe(32);
+});
+
+test('Should convert fahrenheit to celsius', () => {
+    const degree = fahrenheitToCelsius(32);
+    expect(degree).toBe(0);
+});
+
+
+
+
 
 // Why Test?
 // - Saves time
