37a33cc8725f444aa9ec62518dcae548ed8418da
grimgoon <alexander@hermansson.us>
2019-05-29 13:32:59 +0200

    Lecture 147

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -97,3 +97,34 @@ test('Should not delete profile for unauthenticated user', async () => {
         .expect(401)
 });
 
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar','tests/fixtures/image.png')
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+    const response = await request(app)
+        .patch('/users/me').send({
+            name: 'Alexander'
+        })
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .expect(200);
+
+    const user = await User.findById(userOneId);
+    expect(user.name).toBe(response.body.userObject.name);
+});
+
+test('Should not update invalid user fields', async () => {
+    const response = await request(app)
+        .patch('/users/me').send({
+            location: 'Sweden'
+        })
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .expect(400);
+});
