a31bffa7792343363d3d9d8dcbd7a0b86ece8c6b
grimgoon <alexander@hermansson.us>
2019-06-10 08:34:59 +0200

    Lecture 154

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
 const port = process.env.PORT || 5000;
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New Web Socket Connection');
+});
+
+server.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
