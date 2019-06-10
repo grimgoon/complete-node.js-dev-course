a85a1d5f23ddc99ce49d63b25c85cb520eb49d0f
grimgoon <alexander@hermansson.us>
2019-06-10 14:22:18 +0200

    Lecture 157

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -13,9 +13,14 @@ io.on('connection', (socket) => {
     console.log('New Web Socket Connection');
 
     socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left');        
     });
 });
 
