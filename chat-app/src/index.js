b612c3c26205c24e33d10fdcdb02c06d5462321d
grimgoon <alexander@hermansson.us>
2019-06-10 15:50:39 +0200

    Lecture 159

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -15,17 +16,27 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!');
     socket.broadcast.emit('message', 'A new user has joined!');
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter();
+        
+
+        if(filter.isProfane(message)) {
+            return callback('Profanity is not allowed');
+        }
+        
         io.emit('message', message);
+        callback();
+
     });
 
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left');        
     });
 
-    socket.on('sendLocation', (coords) => {
+    socket.on('sendLocation', (coords, callback) => {
         console.log(coords);
         io.emit('message', `https://google.com/maps/?q=${coords.latitude},${coords.longitude}`)
+        callback();
     }); 
 });
 
