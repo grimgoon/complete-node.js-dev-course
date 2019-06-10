2c328031f5b8c5f61225f2d9e36947d5f7c6a958
grimgoon <alexander@hermansson.us>
2019-06-10 16:05:59 +0200

    Lecture 160

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,13 +1,24 @@
 const socket = io();
 
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', (message1) => {
     console.log(message1);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    $messageFormButton.setAttribute('disabled', 'disabled');
+
     const message = e.target.elements.message.value;
     socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
         if(error) {
             return console.log(error);
         }
@@ -16,16 +27,19 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     });
 });
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
+    $sendLocationButton.setAttribute('disabled', 'disabled');
     if(!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.');
     }
 
     navigator.geolocation.getCurrentPosition((position) => {
+
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $sendLocationButton.removeAttribute('disabled');
             console.log('Location Shared!');
         });
     });
