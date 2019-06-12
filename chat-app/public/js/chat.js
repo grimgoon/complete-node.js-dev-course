0bcf106f08f0d9579052b5778a6c36f6ce3ade30
grimgoon <alexander@hermansson.us>
2019-06-12 09:04:43 +0200

    Lecture 165

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,65 +1,68 @@
-const socket = io();
+const socket = io()
 
-const $messageForm = document.querySelector('#message-form');
-const $messageFormInput = $messageForm.querySelector('input');
-const $messageFormButton = $messageForm.querySelector('button');
-const $sendLocationButton = document.querySelector('#send-location');
-const $messages = document.querySelector('#messages');
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
 
-//Templates
-const $messageTemplate = document.querySelector('#message-template').innerHTML;
-const $messageLocationTemplate = document.querySelector('#locationMessage-template').innerHTML; 
-
-const timeFormat = 'HH:mm';
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
-    console.log(message);
-    const html = Mustache.render($messageTemplate, {
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
         message: message.text,
-        createdAt: moment(message.createdAt).format(timeFormat)
-    });
-    $messages.insertAdjacentHTML('beforeend',html);
-});
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
 
-socket.on('locationMessage', (obj) => {
-    const html = Mustache.render($messageLocationTemplate, {
-        url: obj.url,
-        createdAt: moment(obj.createdAt).format(timeFormat)
-    });
-    $messages.insertAdjacentHTML('beforeend',html);
-});
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
 
 $messageForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    $messageFormButton.setAttribute('disabled', 'disabled');
+    e.preventDefault()
 
-    const message = e.target.elements.message.value;
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = e.target.elements.message.value
+
     socket.emit('sendMessage', message, (error) => {
-        $messageFormButton.removeAttribute('disabled');
-        $messageFormInput.value = '';
-        $messageFormInput.focus();
-        if(error) {
-            return console.log(error);
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
         }
 
-        console.log('Message delivered');
-    });
-});
+        console.log('Message delivered!')
+    })
+})
 
 $sendLocationButton.addEventListener('click', () => {
-    $sendLocationButton.setAttribute('disabled', 'disabled');
-    if(!navigator.geolocation) {
-        return alert('Geolocation is not supported by your browser.');
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
     }
 
-    navigator.geolocation.getCurrentPosition((position) => {
+    $sendLocationButton.setAttribute('disabled', 'disabled')
 
+    navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
-            $sendLocationButton.removeAttribute('disabled');
-            console.log('Location Shared!');
-        });
-    });
-});
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location shared!')  
+        })
+    })
+})
